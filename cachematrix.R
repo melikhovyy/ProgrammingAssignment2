a49c3a3bdd26083336831bf367d3e055cd855725
Yevgen Melikhov <melikhovyy@yahoo.com>
2016-09-01 12:45:14 +0200

    saving functions and adding detalied comments to both of them

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,19 @@
 ## The functions <makeCacheMatrix> and <cacheSolve> are designed to create a special
 ## object that stores a numeric matrix and caches its inverse.
 ##
+####################################################################################
+## Example of usage:
+##  source("cachematrix.R")                                         # load R-file
+##  myMatrix <- makeCacheMatrix( matrix(rnorm(100*100), 100, 100) ) # create 100-by-100 matrix
+##  myMatrix$get()                                                  # show the created matrix
+##  myMatrix$getinverse()                                           # no inverse matrix yet
+##  myMatrix$set( matrix(rnorm(100*100), 100, 100) )                # set new 100-by-100 matrix (only for existing)
+##  myMatrix$setinverse( matrix(rnorm(100*100), 100, 100) )         # will fail as there is a check built in
+##  cacheSolve(myMatrix)                                            # create an inverse matrix (invisible mode)
+##  myMatrix$getinverse()                                           # show the inverse matrix
+##  cacheSolve(myMatrix)                                            # show the stored inverse matrix (invisible mode)
+##  isSymmetric.matrix( myMatrix$get() %*% myMatrix$getinverse() )  # confirm that inverse matrix is found
+####################################################################################
 
 ## The function <makeCacheMatrix> creates an object, i.e. a list of 4 functions to 
 ## 1) set the value of the matrix
@@ -19,12 +32,17 @@ makeCacheMatrix <- function(x = matrix()) {
   ## get a stored matrix
   get <- function() x
   ## set a new inverse matrix but after a check that this new matrix is indeed
-  ## an inverse matrix of the stored matrix:
-  ## 
-  setinverse <- function(newValueForInverse) {
-    if ( isSymmetric.matrix( x %*% newValueForInverse ) ) {
+  ## an inverse matrix of the stored matrix
+  setinverse <- function(newValueForInverse, isCheckNeeded=TRUE) {
+    if (isCheckNeeded) {
+      if ( isSymmetric.matrix( x %*% newValueForInverse ) ) {
+        inversedMatrix <<- newValueForInverse
+      } else {
+        message("setinverse() failed as the provided matrix is not an inverse matrix of the existing")
+      }  
+    } else {
       inversedMatrix <<- newValueForInverse
-    } else { message("setinverse() failed as the provided matrix is not an inverse matrix of the existing") }
+    }
   }
   ## get a stored inverse matrix
   getinverse <- function() inversedMatrix
@@ -43,15 +61,22 @@ makeCacheMatrix <- function(x = matrix()) {
 ## created by <makeCacheMatrix>
 ## 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  ##
+  ## get theinverse matrix of 'x'
   inversedMatrix <- x$getinverse()
+  ## check if it is not a a NULL inverse matrix, i.e. if it exists
   if (sum(dim(inversedMatrix))!=0) {
+    ## if it exists, return stored inverse matrix after a corresponding message
+    ## and exit the function 
     message("getting cached data")
-    return(inversedMatrix)
+    return( invisible(inversedMatrix) )
   }
+  ## inverse matrix does not exist, therefore, it needs to be calculated
+  ## get the stored data matrix
   dataMatrix <- x$get()
+  ## inverse the stored data matrix assuming that it is a square invertible matrix
   inversedMatrix <- solve(dataMatrix, ...)
-  x$setinverse(inversedMatrix)
-  inversedMatrix
+  ## set the inverse matrix but do not perform check (check has been done by solve() )
+  x$setinverse(inversedMatrix, isCheckNeeded=FALSE)
+  # return the inverse matrix (add invisible if needed)
+  invisible(inversedMatrix)
 }
